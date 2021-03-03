7b47874434387c922c4da64876b4e0d2ad4bfd91
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2021-03-02 19:37:02 -0500

    Testing action

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
+    const github_token: string = core.getInput('github_token')
+    const workflow_run_id: string = core.getInput('workflow_run_id')
+    core.debug(`github_token: ${github_token} `) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.debug(`workflow_run_id: ${workflow_run_id} `) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    console.log(`github_token: ${github_token} `)
+    console.log(`workflow_run_id: ${workflow_run_id} `)
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
