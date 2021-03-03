e103d6debe15be6baccea2e3c185a82758cb04fb
Mostefa Kamal Lala <mostefa.kamal-lala@cactusoft.ca>
2021-03-02 19:42:18 -0500

    Regenerated

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
@@ -7,8 +6,7 @@ async function run(): Promise<void> {
     const workflow_run_id: string = core.getInput('workflow_run_id')
     core.debug(`github_token: ${github_token} `) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     core.debug(`workflow_run_id: ${workflow_run_id} `) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    console.log(`github_token: ${github_token} `)
-    console.log(`workflow_run_id: ${workflow_run_id} `)
+
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
